c371252f8e5074465c571b9551e6aeeaf634e34f
Niamh Sheehan <72402500+niamhnishiochain@users.noreply.github.com>
2025-07-08 16:21:46 +0100

    Update st_dbscan.py
    note issue with 'precomputed' not sure on solution yes

--- src/st_dbscan/st_dbscan.py
+++ src/st_dbscan/st_dbscan.py
@@ -10,7 +10,6 @@ import numpy as np
 from scipy.spatial.distance import pdist, squareform
 from sklearn.cluster import DBSCAN
 from sklearn.utils import check_array
-from scipy.sparse import coo_matrix
 from sklearn.neighbors import NearestNeighbors
 import warnings
 
@@ -83,22 +82,25 @@ class ST_DBSCAN():
         """
         # check if input is correct
         X = check_array(X)
+        if X.shape[1] < 3:
+            raise ValueError("Input must have at least 3 columns: time, x, y.")
+       
         time = X[:, 0].reshape(-1, 1)
         space = X[:, 1:3] # Assumes x, y
-        covars = X[:, 3:] # Remaining columns as covariates
+        covars = X[:, 3:] if X.shape[1] > 3 else None # Remaining columns as covariates or not!
 
         if not self.eps1 > 0.0 or not self.eps2 > 0.0 or not self.min_samples > 0.0:
             raise ValueError('eps1, eps2, minPts must be positive')
 
-        n, m = X.shape
-
         if len(X) < 20000:
-            # compute with quadratic memory consumption
-
             # Compute sqaured form Euclidean Distance Matrix for 'time' attribute and the spatial attributes
             time_dist = pdist(time, metric=self.metric)
             space_dist = pdist(space, metric=self.metric)
-            covar_dist = pdist(covars, metric=self.metric)
+
+            if covars is not None and covars.shape[1] > 0:
+                covar_dist = pdist(covars, metric=self.metric)
+            else:
+                covar_dist = np.zeros_like(time_dist)
 
             # Only keep distances where all criteria are met
             valid = (time_dist <= self.eps2) & \
@@ -124,7 +126,7 @@ class ST_DBSCAN():
                 nn_spatial = NearestNeighbors(metric=self.metric,
                                               radius=self.eps1)
                 nn_spatial.fit(space)
-                euc_sp = nn_spatial.radius_neighbors_graph(space,
+                space_sp = nn_spatial.radius_neighbors_graph(space,
                                                            mode='distance')
 
                 # Compute sparse matrix for temporal distance
@@ -135,33 +137,53 @@ class ST_DBSCAN():
                                                          mode='distance')
                 
                 # Compute sparse matrix for covariate distance
-                nn_covars = NearestNeighbors(metric=self.metric,
+                if covars is not None and covars.shape[1] > 0:
+                    nn_covars = NearestNeighbors(metric=self.metric,
                                              radius=self.eps3)
-                nn_covars.fit(covars)
-                covar_sp = nn_covars.radius_neighbors_graph(covars,                
+                    nn_covars.fit(covars)
+                    covar_sp = nn_covars.radius_neighbors_graph(covars,                
                                                             mode='distance')
+                else:
+                    covar_sp = time_sp.copy()
+                    # idea being no constraint here but not sure
+                    covar_sp.data.fill(0.0)
                 
                 # Combine the sparse matrices where all three conditions are met
-                non_zero_all_three = time_sp.multiply(euc_sp).multiply(covar_sp)
-
-                # combine both sparse matrixes and filter by time distance matrix
-                row = time_sp.nonzero()[0]
-                column = time_sp.nonzero()[1]
-                v = np.array(euc_sp[row, column])[0]
-
-                # create sparse distance matrix
-                dist_sp = coo_matrix((v, (row, column)), shape=(n, n))
-                dist_sp = dist_sp.tocsc()
-                dist_sp.eliminate_zeros()
+                combined_graph = time_sp.multiply(space_sp).multiply(covar_sp)
 
                 db = DBSCAN(eps=self.eps1,
                             min_samples=self.min_samples,
-                            metric='precomputed')
-                db.fit(dist_sp)
+                            metric='precomputed') # THIS IS WRONG I THINK! 
+                # If 'precomputed' it expects actual distances, but now we have the product of the
+                # distances
+                # XNTS: FIGURE THIS OUT PLEASE  
+                db.fit(combined_graph)
 
                 self.labels = db.labels_
 
         return self
+
+    def _merge_labels(self, prev_labels, new_labels, overlap_len):
+        prev_overlap = prev_labels[-overlap_len:]
+        new_overlap = new_labels[:overlap_len]
+        mapper = {}
+
+        for old, new in zip(prev_overlap, new_overlap):
+            if new != -1 and new not in mapper:
+                mapper[new] = old
+        mapper[-1] = -1
+
+        new_cluster_ids = set(new_labels)
+        overlap_ids = set(new_overlap)
+        unused_ids = new_cluster_ids - overlap_ids
+
+        label_counter = max(prev_labels) + 1 if len(prev_labels) > 0 else 0
+        for cid in unused_ids:
+            if cid != -1:
+                mapper[cid] = label_counter
+                label_counter += 1
+
+        return np.array([mapper[cid] for cid in new_labels])
 
     def fit_frame_split(self, X, frame_size, frame_overlap=None):
         """
@@ -186,77 +208,42 @@ class ST_DBSCAN():
         # check if input is correct
         X = check_array(X)
 
-        # default values for overlap
-        if frame_overlap == None:
+        if frame_overlap is None:
             frame_overlap = self.eps2
+
+        if frame_size <= 0 or frame_overlap <= 0 or frame_size < frame_overlap:
+            raise ValueError('frame_size and/or frame_overlap not correct.')
 
         if not self.eps1 > 0.0 or not self.eps2 > 0.0 or not self.min_samples > 0.0:
             raise ValueError('eps1, eps2, minPts must be positive')
 
-        if not frame_size > 0.0 or not frame_overlap > 0.0 or frame_size < frame_overlap:
-            raise ValueError(
-                'frame_size, frame_overlap not correctly configured.')
-
         # unique time points
-        time = len(X)#np.unique(X[:, 0]) # ?
-
+        time = X[:, 0]
+        unique_times = np.unique(X[:, 0])
         labels = None
-        right_overlap = 0
-        max_label = 0
-        exit = False
+        i = 0
 
-        for i in range(0, time, (frame_size - frame_overlap + 1)):
-            if(exit):
-                break
-            for period in [X[i:i + frame_size]]:
-                frame = X[np.isin(X[:, 0], period)]
+        while i< len(unique_times):
+            start_time = unique_times[i]
+            end_time = start_time + frame_size
 
-                self.fit(frame)
+            frame_mask = (time >= start_time) & (time <= end_time)
+            frame = X[frame_mask]
+            self.fit(frame)
 
-                # match the labels in the overlaped zone
-                # objects in the second frame are relabeled
-                # to match the cluster id from the first frame
-                if not type(labels) is np.ndarray:
-                    labels = self.labels
-                else:
-                    frame_one_overlap_labels = labels[len(labels) -
-                                                      right_overlap:]
-                    frame_two_overlap_labels = self.labels[0:right_overlap]
+            current_labels = self.labels
 
-                    mapper = {}
-                    for i in list(
-                            zip(frame_one_overlap_labels,
-                                frame_two_overlap_labels)):
-                        mapper[i[1]] = i[0]
-                    mapper[
-                        -1] = -1  # avoiding outliers being mapped to cluster
+            if labels is None:
+                labels = current_labels
+            else:
+                overlap_start = end_time - frame_overlap
+                overlap_mask = (time >= overlap_start) & (time <= end_time)
+                overlap_len = np.sum(overlap_mask & frame_mask)
 
-                    # clusters without overlapping points are given new cluster
-                    ignore_clusters = set(
-                        self.labels) - set(frame_two_overlap_labels)
-                    # recode them to new cluster value
-                    if -1 in labels:
-                        labels_counter = len(set(labels)) - 1
-                    else:
-                        labels_counter = len(set(labels))
-                    for j in ignore_clusters:
-                        mapper[j] = labels_counter
-                        labels_counter += 1
+                remapped_labels = self._merge_labels(labels, current_labels, overlap_len)
+                labels = np.concatenate([labels[:-overlap_len], remapped_labels])
 
-                    # objects in the second frame are relabeled to match the cluster id from the first frame
-                    # objects in clusters with no overlap are assigned to new clusters
-                    new_labels = np.array([mapper[j] for j in self.labels])
-
-                    # delete the right overlap
-                    labels = labels[0:len(labels) - right_overlap]
-                    # change the labels of the new clustering and concat
-                    labels = np.concatenate((labels, new_labels))
-                    if(len(labels) == len(X)):
-                        exit = True
-                        break
-
-                right_overlap = len(X[np.isin(X[:, 0],
-                                              period[-frame_overlap + 1:])])
+            i += frame_size - frame_overlap
 
         self.labels = labels[:len(X)]
         return self
